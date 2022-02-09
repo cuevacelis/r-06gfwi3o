b93fd016ee138fb935a3fa323c9db6b8b1eb8da9
cuevacelis <cuevacelis@hotmail.com>
2022-02-08 19:56:30 -0500

    finish challenge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import Welcome from "./Welcome";
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      names: ["Juan", "Pedro", "Germán"]
-    }
+      names: ["Juan", "Pedro", "Germán"],
+    };
   }
 
   render() {
     return (
       <div>
-        {this.state.names.map((name) =>
-          <Welcome name={name} />
-        )}
+        {this.state.names.map((name) => (
+          <Welcome key={name} name={name} />
+        ))}
       </div>
     );
   }
